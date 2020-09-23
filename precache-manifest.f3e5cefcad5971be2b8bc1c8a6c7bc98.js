8e64df9eefe5ef77cf8aebb7a231584bde291c47
duser6062@gmail.com <duser6062@gmail.com>
2020-09-23 17:21:24 +0530

    Updates

---
+++ precache-manifest.f3e5cefcad5971be2b8bc1c8a6c7bc98.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "22b01a08c2ae9016a2cbcb7ea1d28a29",
+    "url": "/index.html"
+  },
+  {
+    "revision": "0313666fd86ab6d3e453",
+    "url": "/static/css/main.5f361e03.chunk.css"
+  },
+  {
+    "revision": "deee1fdf23064d57a8df",
+    "url": "/static/js/2.503196c3.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/static/js/2.503196c3.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "0313666fd86ab6d3e453",
+    "url": "/static/js/main.47992dd7.chunk.js"
+  },
+  {
+    "revision": "1986fb9401d103faf8ed",
+    "url": "/static/js/runtime-main.f78ad7b6.js"
+  },
+  {
+    "revision": "5d5d9eefa31e5e13a6610d9fa7a283bb",
+    "url": "/static/media/logo.5d5d9eef.svg"
+  }
+]);
