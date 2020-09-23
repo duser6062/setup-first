1d2ca22c6a501616afceef3807631b67acdf4b5b
duser6062@gmail.com <duser6062@gmail.com>
2020-09-23 17:47:08 +0530

    react commit

---
+++ build/precache-manifest.c48b60acdcb2367502f72c25aac360d3.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "f8085841f139d49987323f75a6e29457",
+    "url": "/setup-first/index.html"
+  },
+  {
+    "revision": "66ebab38e1eb7718a49b",
+    "url": "/setup-first/static/css/main.5f361e03.chunk.css"
+  },
+  {
+    "revision": "90e52c57d37746d906fc",
+    "url": "/setup-first/static/js/2.763b34e5.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/setup-first/static/js/2.763b34e5.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "66ebab38e1eb7718a49b",
+    "url": "/setup-first/static/js/main.c4212782.chunk.js"
+  },
+  {
+    "revision": "8dbeaa63c21433e9c6ab",
+    "url": "/setup-first/static/js/runtime-main.d1336569.js"
+  },
+  {
+    "revision": "5d5d9eefa31e5e13a6610d9fa7a283bb",
+    "url": "/setup-first/static/media/logo.5d5d9eef.svg"
+  }
+]);
