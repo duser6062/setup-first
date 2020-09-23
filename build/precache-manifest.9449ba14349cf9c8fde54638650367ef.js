d6209993f93a06d57c9ff5077bcbc18517d0e7ee
duser6062@gmail.com <duser6062@gmail.com>
2020-09-23 18:06:10 +0530

    react commit

---
+++ build/precache-manifest.9449ba14349cf9c8fde54638650367ef.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "feb73426c0d8fce47fb82cc39cc5ab51",
+    "url": "/setup-first/build/index.html"
+  },
+  {
+    "revision": "d5f473a02f515d2367f9",
+    "url": "/setup-first/build/static/css/main.5f361e03.chunk.css"
+  },
+  {
+    "revision": "90e52c57d37746d906fc",
+    "url": "/setup-first/build/static/js/2.763b34e5.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/setup-first/build/static/js/2.763b34e5.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "d5f473a02f515d2367f9",
+    "url": "/setup-first/build/static/js/main.19935e12.chunk.js"
+  },
+  {
+    "revision": "93a74fad7f2f5884f89e",
+    "url": "/setup-first/build/static/js/runtime-main.e106ab61.js"
+  },
+  {
+    "revision": "5d5d9eefa31e5e13a6610d9fa7a283bb",
+    "url": "/setup-first/build/static/media/logo.5d5d9eef.svg"
+  }
+]);
--- build/service-worker.js
+++ build/service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/4.3.1/workbox-sw.js");
 
 importScripts(
-  "/setup-first/precache-manifest.c48b60acdcb2367502f72c25aac360d3.js"
+  "/setup-first/build/precache-manifest.9449ba14349cf9c8fde54638650367ef.js"
 );
 
 self.addEventListener('message', (event) => {
@@ -33,7 +33,7 @@ workbox.core.clientsClaim();
 self.__precacheManifest = [].concat(self.__precacheManifest || []);
 workbox.precaching.precacheAndRoute(self.__precacheManifest, {});
 
-workbox.routing.registerNavigationRoute(workbox.precaching.getCacheKeyForURL("/setup-first/index.html"), {
+workbox.routing.registerNavigationRoute(workbox.precaching.getCacheKeyForURL("/setup-first/build/index.html"), {
   
   blacklist: [/^\/_/,/\/[^\/?]+\.[^\/]+$/],
 });
